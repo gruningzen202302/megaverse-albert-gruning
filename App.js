524ad2bed8899b28262df5ed3f2601e317a151d5
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-10-15 06:49:32 -0300

    feat: declare milkyway emoji to create starting scenario

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const milky = "\u{1F30C}";
+
+let array = [];
+let matrix = [];
+
+for (let i = 0; i < 11; i++) {
+  array.push(milky);
+}
+for (let i = 0; i < 11; i++) {
+  matrix.push(array);
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
+      <Text style={styles.text}>{matrix[0]}</Text>
+      <Text style={styles.text}>{matrix[1]}</Text>
+      <Text style={styles.text}>{matrix[2]}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,7 +33,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#FFD700",
-    fontSize: 20,
+    fontSize: 15,
     fontWeight: "bold",
   },
 });
