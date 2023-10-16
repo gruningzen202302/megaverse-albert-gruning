cd5867d6c595aa766196f969d5e89ccd07e81037
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-10-15 21:17:04 -0300

    refactor: rename variables to make the code self explainatory

--- App.js
+++ App.js
@@ -2,23 +2,41 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 const milky = "\u{1F30C}";
+const planet = "\u{1FA90}";
+const columns = 11;
+const rows = 11;
 
-let array = [];
 let matrix = [];
 
-for (let i = 0; i < 11; i++) {
-  array.push(milky);
-}
-for (let i = 0; i < 11; i++) {
-  matrix.push(array);
-}
+const drawpixel = (arr, mil) => arr.push(mil);
+const drawRaw = (mat, row) => mat.push(row);
+
+const drawLine = (emoji, columns) => {
+  let line = [];
+  for (let position = 0; position < columns; position++) {
+    drawpixel(line, emoji);
+  }
+  return line;
+};
+
+const milkyLine = drawLine(milky, columns);
+const planetLine = drawLine(planet, columns);
+
+const drawGrid = () => {
+  for (let i = 0; i < rows; i++) {
+    if (i % 2 === 0) {
+      drawRaw(matrix, milkyLine);
+    } else {
+      drawRaw(matrix, planetLine);
+    }
+  }
+};
+drawGrid();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{matrix[0]}</Text>
-      <Text style={styles.text}>{matrix[1]}</Text>
-      <Text style={styles.text}>{matrix[2]}</Text>
+      <Text style={styles.text}>{matrix}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -30,6 +48,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#307BAA",
     alignItems: "center",
     justifyContent: "center",
+    padding: "5%",
   },
   text: {
     color: "#FFD700",
