f6530dab0cbb8089fa50b97aaf18c9050e836c31
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-10-15 21:53:04 -0300

    refactor: isolate the draw strategy to develop the challenge pattern

--- App.js
+++ App.js
@@ -2,44 +2,55 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 const milky = "\u{1F30C}";
-const planet = "\u{1FA90}";
-const columns = 11;
-const rows = 11;
+const planet = "\u{1FA90}"
 
-let matrix = [];
+const emoji = {
+  milky: milky,
+  planet: planet,
+}
 
-const drawpixel = (arr, mil) => arr.push(mil);
-const drawRaw = (mat, row) => mat.push(row);
+const columns = 11
+const rows = 11
+
+let matrix = []
+
+const drawpixel = (arr, mil) => arr.push(mil)
+const drawRaw = (mat, row) => mat.push(row)
 
 const drawLine = (emoji, columns) => {
-  let line = [];
+  let line = []
   for (let position = 0; position < columns; position++) {
-    drawpixel(line, emoji);
+    drawpixel(line, emoji)
   }
-  return line;
-};
+  return line
+}
 
-const milkyLine = drawLine(milky, columns);
-const planetLine = drawLine(planet, columns);
+const milkyLine = drawLine(emoji.milky, columns)
+const planetLine = drawLine(emoji.planet, columns)
 
-const drawGrid = () => {
-  for (let i = 0; i < rows; i++) {
-    if (i % 2 === 0) {
-      drawRaw(matrix, milkyLine);
+const drawStrategy = (rowCount, grid, emojis) => {
+  for (let rawPosition = 0; rawPosition < rowCount; rawPosition++) {
+    if (rawPosition % 2 === 0) {
+      drawRaw(grid, emojis[0])
     } else {
-      drawRaw(matrix, planetLine);
+      drawRaw(grid, emojis[1])
     }
   }
-};
-drawGrid();
+}
+const drawGrid = (rowCount, emojis) => {
+  let grid = []
+  drawStrategy(rows, grid, emojis)
+  return grid
+}
+const grid = drawGrid(rows, [milkyLine, planetLine])
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{matrix}</Text>
+      <Text style={styles.text}>{grid}</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -55,4 +66,5 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: "bold",
   },
-});
+})
+
