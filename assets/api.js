c6109e728c08db185868045bcd9a90123c0ba651
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-10-16 18:38:46 -0300

    fix: remove unused code to avoid fetch repetitions

--- assets/api.js
+++ assets/api.js
@@ -2,7 +2,6 @@ const Api = {
   btcUrl: "https://api.coindesk.com/v1/bpi/currentprice.json",
   errorTestUrl:
     "this url does not exist expected TypeError Network request failed",
-  get: fetch("https://api.coindesk.com/v1/bpi/currentprice.json"),
   post: "post endpoint",
 }
 
