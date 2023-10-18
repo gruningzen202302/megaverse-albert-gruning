9083c4a7527431e82181828e4b6131a36c665018
vir-th-w11 <gruningzen202302@gmail.com>
2023-10-18 00:24:01 -0300

    test: call a get endpoint to test fetch from api module

--- App.js
+++ App.js
@@ -65,7 +65,7 @@ export default function App() {
     console.warn("testing API GET effects")
   }
   useEffect(() => {
-    Apiget()
+    Api.getApiData()
   }, [])
 
   const getContent = () => {
--- api.js
+++ api.js
@@ -5,14 +5,21 @@ const baseUrl = "https://challenge.crossmint.io/api"
 const polyanets = "polyanets"
 const soloons = "soloons"
 const comeths = "comeths"
+const btcUrl = "https://api.coindesk.com/v1/bpi/currentprice.json"
+const placeHolderUrl = "https://jsonplaceholder.typicode.com/posts/1"
 
 const Api = {
-  btcUrl: "https://api.coindesk.com/v1/bpi/currentprice.json",
+  btcUrl: btcUrl,
   errorTestUrl:
     "this url does not exist expected TypeError Network request failed",
   polyanets: baseUrl + "/" + polyanets,
   soloons: baseUrl + "/" + soloons,
   comeths: baseUrl + "/" + comeths,
+  getApiData: async () => {
+    let res = await fetch(placeHolderUrl)
+    let formattedRes = await res.json()
+    console.warn(formattedRes?.title)
+  },
 }
 
 export default Api
